cac9a86e0b34d492072ed241277166fc5a664dd0
TurtleWolfe <JonPohlner@gmail.com>
2021-01-13 15:32:18 -0500

    D3BarChart

--- dist/javaScript/javaScript.js
+++ dist/javaScript/javaScript.js
@@ -1,104 +1,122 @@
-let runs = [
-  {
-    id: 1,
-    date: 'October 1, 2017 at 4:00PM',
-    distance: 5.2
-  },
-  {
-    id: 2,
-    date: 'October 2, 2017 at 5:00PM',
-    distance: 7.0725
-  },
-  {
-    id: 3,
-    date: 'October 3, 2017 at 6:00PM',
-    distance: 8.7
-  },
-  {
-    id: 4,
-    date: 'October 4, 2017 at 4:00PM',
-    distance: 5.2
-  },
-  {
-    id: 5,
-    date: 'October 5, 2017 at 5:00PM',
-    distance: 7.0725
-  },
-  {
-    id: 6,
-    date: 'October 6, 2017 at 6:00PM',
-    distance: 8.7
-  }
-];
+var WIDTH = 800;
+var HEIGHT = 600;
 
-// let WIDTH = 800;
-// let HEIGHT = 600;
-
-let WIDTH = 800;
-let HEIGHT = 300;
-
+// set width and height of svg
 d3.select('svg')
-  .style('WIDTH', WIDTH)
-  .style('HEIGHT', HEIGHT);
+  .style('width', WIDTH)
+  .style('height', HEIGHT);
 
-d3.json('data.json').then(function (data) {
-  // console.log(data);
-  d3.select('svg').selectAll('rect')
-    .data(data)
-    .enter()
-    .append('rect');
+// fetch data
+// // section 4 Making an AJAX request
 
-  var yScale = d3.scaleLinear();
-  yScale.range([HEIGHT, 0]);
-  var yMin = d3.min(data, function (datum, index) {
-    return datum.count;
-  });
-  var yMax = d3.max(data, function (datum, index) {
-    return datum.count;
-  });
-  yScale.domain([yMin - 1, yMax]);
+d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json').then(function (data) {
 
-  d3.selectAll('rect')
-    .attr('height', function (datum, index) {
-      return HEIGHT - yScale(datum.count);
-    });
+  console.log(data);
+  console.log(data.data);
+  data = data.data;
+  console.log(data);
+  //once data is retrieved...
 
-  var xScale = d3.scaleLinear(); xScale.range([0, WIDTH]); xScale.domain([0, data.length]); d3.selectAll('rect').attr('x', function (datum, index) { return xScale(index); });
 
-  d3.selectAll('rect')
-    .attr('y', function (datum, index) {
-      return yScale(datum.count);
-    });
+  // // section 5 Using AJAX data to create SVG elements
 
-  d3.selectAll('rect')
-    .attr('width', WIDTH / data.length);
+  // d3.select('svg').selectAll('rect') //select rectangles within svg (even if none exist)
+  //   .data(data) //attach data to the rectangles
+  //   .enter() //find the data elements that are not attached to rectangles
+  //   .append('rect'); //append rectangles for each data not attached to a rectangle
 
-  var yDomain = d3.extent(data, function (datum, index) {
-    return datum.count;
-  });
-  var colorScale = d3.scaleLinear();
-  colorScale.domain(yDomain);
-  colorScale.range(['#00cc00', 'blue']);
-  d3.selectAll('rect')
-    .attr('fill', function (datum, index) {
-      return colorScale(datum.count);
-    });
 
-  var leftAxis = d3.axisLeft(yScale);
-  d3.select('svg')
-    .append('g').attr('id', 'left-axis')
-    .call(leftAxis);
+  // // section 6 Adjusting the height and the width of the bars
 
-  var skillScale = d3.scaleBand();
-  var skillDomain = data.map(function (skill) {
-    return skill.name;
-  });
-  skillScale.range([0, WIDTH]);
-  skillScale.domain(skillDomain);
+  // var yScale = d3.scaleLinear(); //create a linear scale
+  // yScale.range([HEIGHT, 0]); //set its visual range to 600 -> 0 (remember bottom of svg is 600px down from top)
+  // var yMin = d3.min(data, function (datum, index) { //get the minimum y data value...
+  //   return datum.count; //by looking at the count property of each datum
+  // });
+  // var yMax = d3.max(data, function (datum, index) { //get the maximum y data value...
+  //   return datum.count; //by looking at the count property of each datum
+  // });
+  // yScale.domain([yMin - 1, yMax]); //set the domain of yScale from yMin and yMax
 
-  var bottomAxis = d3.axisBottom(skillScale);
-  d3.select('svg')
-    .append('g').attr('id', 'bottom-axis')
-    .call(bottomAxis)
-    .attr('transform', 'translate(0,' + HEIGHT + ')');
+
+  // // section 6 Adjusting the height and the width of the bars
+
+  // d3.selectAll('rect') //find all rectangles
+  //   .attr('height', function (datum, index) { //set the height of each rectangle...
+  //     //...by getting the count property of each datum
+  //     //converting it to a visual value, using yScale
+  //     //(remember, when using yScale as it is set up, a large data value will give you a small visual value and vice versa)
+  //     //then subtract that value from HEIGHT
+  //     //this will make a large bar for a large data value
+  //     return HEIGHT - yScale(datum.count);
+  //   });
+
+
+  // // section 7 Adjusting the horizontal and the vertical placement of the bars
+
+  // var xScale = d3.scaleLinear(); //create the xScale
+  // xScale.range([0, WIDTH]); //set the range to 0->800
+  // xScale.domain([0, data.length]); //set the domain from 0 to the number of data elements retrieved
+  // d3.selectAll('rect') //select all rectangles
+  //   .attr('x', function (datum, index) { //set the x position of each rectangle...
+  //     return xScale(index);//by converting the index of the elemnt in the array to a point between 0->800
+  //   });
+
+
+  // // section 7 Adjusting the horizontal and the vertical placement of the bars
+
+  // d3.selectAll('rect') //select all rectangles
+  //   .attr('y', function (datum, index) { //set the y position of each rectangle...
+  //     //by converting the count property of the datum to a visual value
+  //     //(remember, when using yScale as it is set up, a large data value will give you a small visual value and vice versa)
+  //     return yScale(datum.count);
+  //   });
+
+
+  // // section 8 Making the width of the bars dynamic
+
+  // d3.selectAll('rect') //select all rectangles
+  //   .attr('width', WIDTH / data.length); //set the width of all rectangles to be the width of the SVG divided by the number of data elements
+
+
+  // // section 9 Changing the color of the bar based on data
+
+  // var yDomain = d3.extent(data, function (datum, index) { //set the y domain by getting the min/max with d3.extent
+  //   return datum.count; //... and examining the count property of each datum
+  // });
+  // var colorScale = d3.scaleLinear();//create a linear scale
+  // colorScale.domain(yDomain); //the domain is the yDomain
+  // colorScale.range(['#00cc00', 'blue']); //the visual range goes from green->blue
+  // d3.selectAll('rect') //select all rectangles
+  //   .attr('fill', function (datum, index) { //set the fill of each rectangle
+  //     return colorScale(datum.count); //by converting the count property of the datum to a color
+  //   });
+
+
+  // // section 10 Adding axes
+
+  // var leftAxis = d3.axisLeft(yScale); //create a left axis generator using the yScale
+  // d3.select('svg') //select the svg
+  //   .append('g').attr('id', 'left-axis') //append a <g> tag to it with id=left-axis
+  //   .call(leftAxis); // create a left axis within that <g>
+
+
+  // // section 10 Adding axes
+
+  // var skillScale = d3.scaleBand(); //create a scale band that will map skills to horizontal positions
+  // var skillDomain = data.map(function (skill) { //create an array of skill strings
+  //   return skill.name;
+  // });
+  // skillScale.range([0, WIDTH]); //set the range of the skillScale to 0->800
+  // skillScale.domain(skillDomain); //set the domain to be the array of skill strings
+
+
+  // // section 10 Adding axes
+
+  // var bottomAxis = d3.axisBottom(skillScale); //create a bottom axis generator that uses the skillScale
+  // d3.select('svg') //select the svg
+  //   .append('g').attr('id', 'bottom-axis') //append a <g> tag to it with id=bottom-axis
+  //   .call(bottomAxis) // create a bottom axis within that <g>
+  //   .attr('transform', 'translate(0,' + HEIGHT + ')'); //move it to the bottom of the svg
+
 });
