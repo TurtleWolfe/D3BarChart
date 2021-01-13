c0a8d4a7fd06e58250e0d4698a182e774719d15b
TurtleWolfe <JonPohlner@gmail.com>
2021-01-13 16:40:43 -0500

    D3BarChart

--- dist/javaScript/javaScript.js
+++ dist/javaScript/javaScript.js
@@ -11,66 +11,67 @@ d3.select('svg')
 
 d3.json('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json').then(function (data) {
 
-  console.log(data);
-  console.log(data.data);
+  // console.log(data);
+  // console.log(data.data);
   data = data.data;
   console.log(data);
   //once data is retrieved...
-
-
   // // section 5 Using AJAX data to create SVG elements
 
-  // d3.select('svg').selectAll('rect') //select rectangles within svg (even if none exist)
-  //   .data(data) //attach data to the rectangles
-  //   .enter() //find the data elements that are not attached to rectangles
-  //   .append('rect'); //append rectangles for each data not attached to a rectangle
+  d3.select('svg').selectAll('rect') //select rectangles within svg (even if none exist)
+    .data(data) //attach data to the rectangles
+    .enter() //find the data elements that are not attached to rectangles
+    .append('rect'); //append rectangles for each data not attached to a rectangle
 
 
   // // section 6 Adjusting the height and the width of the bars
 
-  // var yScale = d3.scaleLinear(); //create a linear scale
-  // yScale.range([HEIGHT, 0]); //set its visual range to 600 -> 0 (remember bottom of svg is 600px down from top)
-  // var yMin = d3.min(data, function (datum, index) { //get the minimum y data value...
-  //   return datum.count; //by looking at the count property of each datum
-  // });
-  // var yMax = d3.max(data, function (datum, index) { //get the maximum y data value...
-  //   return datum.count; //by looking at the count property of each datum
-  // });
-  // yScale.domain([yMin - 1, yMax]); //set the domain of yScale from yMin and yMax
+  var yScale = d3.scaleLinear(); //create a linear scale
+  yScale.range([HEIGHT, 0]); //set its visual range to 600 -> 0 (remember bottom of svg is 600px down from top)
+  var yMin = d3.min(data, function (datum, index) { //get the minimum y data value...
+    // console.log(datum[1]);
+    return datum[1]; //by looking at the count property of each datum
+  });
+  var yMax = d3.max(data, function (datum, index) { //get the maximum y data value...
+    // console.log(datum[1]);
+    return datum[1]; //by looking at the count property of each datum
+  });
+  yScale.domain([yMin - 1, yMax]); //set the domain of yScale from yMin and yMax
 
 
   // // section 6 Adjusting the height and the width of the bars
 
-  // d3.selectAll('rect') //find all rectangles
-  //   .attr('height', function (datum, index) { //set the height of each rectangle...
-  //     //...by getting the count property of each datum
-  //     //converting it to a visual value, using yScale
-  //     //(remember, when using yScale as it is set up, a large data value will give you a small visual value and vice versa)
-  //     //then subtract that value from HEIGHT
-  //     //this will make a large bar for a large data value
-  //     return HEIGHT - yScale(datum.count);
-  //   });
+  d3.selectAll('rect') //find all rectangles
+    .attr('height', function (datum, index) { //set the height of each rectangle...
+      //...by getting the count property of each datum
+      //converting it to a visual value, using yScale
+      //(remember, when using yScale as it is set up, a large data value will give you a small visual value and vice versa)
+      //then subtract that value from HEIGHT
+      //this will make a large bar for a large data value
+      console.log(datum[1]);
+      return HEIGHT - yScale(datum[1]);
+    });
 
 
   // // section 7 Adjusting the horizontal and the vertical placement of the bars
 
-  // var xScale = d3.scaleLinear(); //create the xScale
-  // xScale.range([0, WIDTH]); //set the range to 0->800
-  // xScale.domain([0, data.length]); //set the domain from 0 to the number of data elements retrieved
-  // d3.selectAll('rect') //select all rectangles
-  //   .attr('x', function (datum, index) { //set the x position of each rectangle...
-  //     return xScale(index);//by converting the index of the elemnt in the array to a point between 0->800
-  //   });
+  var xScale = d3.scaleLinear(); //create the xScale
+  xScale.range([0, WIDTH]); //set the range to 0->800
+  xScale.domain([0, data.length]); //set the domain from 0 to the number of data elements retrieved
+  d3.selectAll('rect') //select all rectangles
+    .attr('x', function (datum, index) { //set the x position of each rectangle...
+      return xScale(index);//by converting the index of the elemnt in the array to a point between 0->800
+    });
 
 
   // // section 7 Adjusting the horizontal and the vertical placement of the bars
 
-  // d3.selectAll('rect') //select all rectangles
-  //   .attr('y', function (datum, index) { //set the y position of each rectangle...
-  //     //by converting the count property of the datum to a visual value
-  //     //(remember, when using yScale as it is set up, a large data value will give you a small visual value and vice versa)
-  //     return yScale(datum.count);
-  //   });
+  d3.selectAll('rect') //select all rectangles
+    .attr('y', function (datum, index) { //set the y position of each rectangle...
+      //by converting the count property of the datum to a visual value
+      //(remember, when using yScale as it is set up, a large data value will give you a small visual value and vice versa)
+      return yScale(datum[1]);
+    });
 
 
   // // section 8 Making the width of the bars dynamic
